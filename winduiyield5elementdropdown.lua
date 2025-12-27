c1b47d87776041887b7439ab04bd98f14771c40d
Monthon Sova <igaccmonthon1150@gmail.com>
2025-12-28 01:55:40 +0700

    Update winduiyield5elementdropdown.lua

--- winduiyield5elementdropdown.lua
+++ winduiyield5elementdropdown.lua
@@ -7044,7 +7044,7 @@ local a a={cache={}, load=function(b)if not a.cache[b]then a.cache[b]={c=a[b]()}
     
     -- Yield every 10 items to allow UI to process other tasks (non-blocking)
     if itemCount%5==0 then
-    task.wait(0.1)
+    task.wait(0.05)
     end
     
     ar:Display()
